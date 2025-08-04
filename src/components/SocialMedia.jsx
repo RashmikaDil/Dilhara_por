f94f8129a22494c9e7eb9e610138f28cc71a6ca4
Rashmika Dillhara <rashmikadil2023@gmail.com>
2025-08-04 11:54:51 +0530

    added social media

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,5 +1,5 @@
 
-import { faFacebook, faGithub, faInstagram, faLinkedin, faXTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faInstagram, faLinkedin, faXTwitter } from '@fortawesome/free-brands-svg-icons';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
@@ -9,16 +9,16 @@ const SocialMedia = () => {
     <div className="flex text-white  text-5xl  gap-10 mt-4">
 
 <a href="https://www.instagram.com/rashmikadil523/" target='blank'>
-  <FontAwesomeIcon className='text-amber-50 cursor-pointer hover:text-gray-400 transition-all' icon={faInstagram}></FontAwesomeIcon>
+  <FontAwesomeIcon className='text-amber-50 cursor-pointer hover:text-special transition-all' icon={faInstagram}></FontAwesomeIcon>
 </a>
 <a href="https://x.com/RashmikaDil2023" target='blank'>
-  <FontAwesomeIcon className='text-amber-50 cursor-pointer hover:text-gray-400 transition-all' icon={faXTwitter}></FontAwesomeIcon>
+  <FontAwesomeIcon className='text-amber-50 cursor-pointer hover:text-special transition-all' icon={faXTwitter}></FontAwesomeIcon>
 </a>
 <a href="https://www.linkedin.com/in/rashmika-dilhara-47a7102aa/" target='blank'>
-  <FontAwesomeIcon className='text-amber-50 cursor-pointer hover:text-gray-400 transition-all' icon={faLinkedin}></FontAwesomeIcon>
+  <FontAwesomeIcon className='text-amber-50 cursor-pointer hover:text-special transition-all' icon={faLinkedin}></FontAwesomeIcon>
 </a>
 <a href="https://github.com/RashmikaDil" target='blank'>
-  <FontAwesomeIcon className='text-amber-50 cursor-pointer hover:text-gray-400 transition-all' icon={faGithub}></FontAwesomeIcon>
+  <FontAwesomeIcon className='text-amber-50 cursor-pointer hover:text-special transition-all' icon={faGithub}></FontAwesomeIcon>
 </a>
 
 
